dff7914974a2f5fe947040004eb90ecceafa8a06
viettx <viettx@thinhtientech.com>
2020-11-17 10:40:41 +0700

    first init

--- libs/system-navigator/src/lib/system-navigator.service.ts
+++ libs/system-navigator/src/lib/system-navigator.service.ts
@@ -5,61 +5,64 @@ import { map } from 'rxjs/operators';
 import { AppUrl, ServerResponse } from './models';
 
 export class SystemNavigatorServiceConfig {
-    api: string;
-    headers?: any;
+  api: string;
+  headers?: any;
 }
 
 @Injectable()
 export class SystemNavigatorService {
-    api: string;
-    headers: any;
-    imageToShow: string | ArrayBuffer;
+  api: string;
+  headers: any;
+  imageToShow: string | ArrayBuffer;
 
-    constructor(public http: HttpClient, @Optional() @Inject(SystemNavigatorServiceConfig) config?: SystemNavigatorServiceConfig) {
-        if (config) {
-            this.api = config.api;
-            this.headers = config.headers;
-        }
+  constructor(
+    public http: HttpClient,
+    @Optional() @Inject(SystemNavigatorServiceConfig) config?: SystemNavigatorServiceConfig
+  ) {
+    if (config) {
+      this.api = config.api;
+      this.headers = config.headers;
+    }
+  }
+
+  getUrls(): Observable<ServerResponse> {
+    const headers = new HttpHeaders(this.headers);
+
+    return this.http
+      .get<ServerResponse>(this.api, { headers })
+      .pipe(
+        map((res: ServerResponse) => {
+          res.data.map((appUrl: AppUrl) => {
+            this.createImageFromBlob(appUrl.image);
+            appUrl.image = this.imageToShow;
+            return appUrl;
+          });
+
+          return res;
+        })
+      );
+  }
+
+  private createImageFromBlob(image: Blob | string) {
+    if (!image) {
+      this.imageToShow = undefined;
+      return;
     }
 
-    getUrls(): Observable<ServerResponse> {
-        const headers = new HttpHeaders(this.headers);
-
-        return this.http
-            .get<ServerResponse>(this.api, { headers })
-            .pipe(
-                map((res: ServerResponse) => {
-                    res.data.map((appUrl: AppUrl) => {
-                        this.createImageFromBlob(appUrl.image);
-                        appUrl.image = this.imageToShow;
-                        return appUrl;
-                    });
-
-                    return res;
-                })
-            );
+    if (typeof image === 'string') {
+      this.imageToShow = image;
+      return;
     }
 
-    private createImageFromBlob(image: Blob | string) {
-        if (!image) {
-            this.imageToShow = undefined;
-            return;
-        }
+    const reader = new FileReader();
+    reader.addEventListener(
+      'load',
+      () => {
+        this.imageToShow = reader.result;
+      },
+      false
+    );
 
-        if (typeof image === 'string') {
-            this.imageToShow = image;
-            return;
-        }
-
-        let reader = new FileReader();
-        reader.addEventListener(
-            'load',
-            () => {
-                this.imageToShow = reader.result;
-            },
-            false
-        );
-
-        reader.readAsDataURL(image);
-    }
+    reader.readAsDataURL(image);
+  }
 }
